bbb1cc1bcb5b3ed28ff7daa7a369d5a3a1c533ab
Josh Robbs <josh@rustbeltwebworks.com>
2018-05-15 15:27:41 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,15 @@
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number)
+  }
+  return "done"
+}
+function maybeTrue() {
+    return Math.random() >= 0.5 
+  }
+function doWhileLoop(array){
+  do {
+    array.unshift
+  } while (array.length > 0 )
+  return array
+}
